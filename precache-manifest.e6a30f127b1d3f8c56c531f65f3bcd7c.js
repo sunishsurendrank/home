2b6a69877b3357d2b678be42b1c4dcd3d485e6a4
root <root@lab1.novalocal>
2021-05-09 19:43:51 +0300

    Updates

---
+++ precache-manifest.e6a30f127b1d3f8c56c531f65f3bcd7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49330220e67bbe592735a040afcbbcf9",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "c3d115714a6722966821",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1390d13147fcc8767529",
+    "url": "/home/static/js/2.95a996af.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.95a996af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d115714a6722966821",
+    "url": "/home/static/js/main.921c24f0.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "a22d107c5036b9bdb1a0cb75da7bffd3",
+    "url": "/home/static/media/philips.a22d107c.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "6a27cbdc24c40d3027526fae4ea14cb6",
+    "url": "/home/static/media/sunish.6a27cbdc.png"
+  }
+]);
