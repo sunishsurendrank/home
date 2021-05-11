146cfda78d993e49558c8b98ee905dc90047e023
root <root@lab1.novalocal>
2021-05-11 18:54:23 +0300

    Updates

--- precache-manifest.525681b1f0170a8a1d5fd52fc7a34201.js
+++ precache-manifest.525681b1f0170a8a1d5fd52fc7a34201.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd41bcd16fc630a4cdc2557e84393ca8",
+    "revision": "813e8b96ab09336e7ccc47c637140a5a",
     "url": "/home/index.html"
   },
   {
-    "revision": "8da187fa248e9b74cd10",
+    "revision": "4e976a3fe18ac1e8f79b",
     "url": "/home/static/css/main.f5497eb8.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.3db59def.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8da187fa248e9b74cd10",
-    "url": "/home/static/js/main.d4c1d309.chunk.js"
+    "revision": "4e976a3fe18ac1e8f79b",
+    "url": "/home/static/js/main.33330338.chunk.js"
   },
   {
     "revision": "c0578b487099793a068c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.99939aeee287d33df9138a8654592f8a.js"
+  "/home/precache-manifest.525681b1f0170a8a1d5fd52fc7a34201.js"
 );
 
 self.addEventListener('message', (event) => {
